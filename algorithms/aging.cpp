7a4af515530ff0b609c39ae84bbb2d93421d0f93
MennaMagdie <mennamagdyx@gmail.com>
2024-12-29 23:06:29 +0200

    handled the unknown aging testcases :)

--- algorithms/aging.cpp
+++ algorithms/aging.cpp
@@ -1,54 +1,84 @@
 #include "aging.h"
 
-char* aging(vector<Process>& processes, int endTime, int quantum) {
-    char* times = new char[endTime];
+char* aging(vector<Process> &processes, int endTime, int quantum)
+{
+    char *times = new char[endTime];
     memset(times, '_', endTime);
 
-    vector<Process*> readyQueue; 
-    Process* prevProcess = nullptr;
+    vector<Process *> readyQueue;
+    Process *prevProcess = nullptr;
 
-    for (int currentTime = 0; currentTime < endTime; currentTime++) {
-        
-        for (auto& process : processes) {
-            if (process.arrivalTime == currentTime) {
+    for (int currentTime = 0; currentTime < endTime; currentTime++)
+    {
+        for (auto &process : processes)
+        {
+            if (process.arrivalTime == currentTime)
+            {
                 process.priority = process.third_attribute;
-                process.lastExecuted = currentTime;  
+                process.lastExecuted = currentTime;
                 readyQueue.push_back(&process);
             }
         }
 
-        if (!readyQueue.empty()) {
+        if (!readyQueue.empty())
+        {
 
-            Process* highestPriorityProcess = nullptr;
-            for (auto& process : readyQueue) {
-                if(process != prevProcess) {
+            Process *highestPriorityProcess = nullptr;
+            for (auto &process : readyQueue)
+            {
+                if (process != prevProcess)
+                {
                     process->priority++;
                     // cout <<" t = " << currentTime << " | "<< process->name <<" = "<< process->priority << endl;
                 }
-                if (highestPriorityProcess == nullptr || process->priority > highestPriorityProcess->priority) {
+                if (highestPriorityProcess == nullptr || process->priority > highestPriorityProcess->priority)
+                {
                     highestPriorityProcess = process;
                 }
-                else if (process->priority == highestPriorityProcess->priority) {
-                    if (process->lastExecuted < highestPriorityProcess->lastExecuted) {
+                else if (process->priority == highestPriorityProcess->priority)
+                {
+                    if (process->lastExecuted < highestPriorityProcess->lastExecuted)
+                    {
                         highestPriorityProcess = process;
                     }
                 }
             }
 
-            Process* selectedProcess = highestPriorityProcess;
+            Process *selectedProcess = highestPriorityProcess;
 
-            times[currentTime] = selectedProcess->name;
-            selectedProcess->lastExecuted = currentTime + 1;
+            int runTime = min(quantum, endTime - currentTime);
+            for (int t = 0; t < runTime; t++)
+            {
+                if (currentTime + t < endTime)
+                {
+                    times[currentTime + t] = selectedProcess->name;
+                    if (t != 0)
+                    {
+                        for (auto &process : readyQueue)
+                        {
+                            if (process != selectedProcess)
+                            {
+                                process->priority++;
+                                // cout <<" t = " << currentTime << " | "<< process->name <<" = "<< process->priority << endl;
+                            }
+                        }
+                    }
+                }
+            }
+
+            selectedProcess->lastExecuted = currentTime + runTime;
             selectedProcess->priority = selectedProcess->third_attribute;
-            prevProcess = selectedProcess;
 
+
+            currentTime += runTime - 1; 
+
+            prevProcess = selectedProcess;
             // for (auto& process : readyQueue) {
             //     if (process != selectedProcess) {
             //         process->priority++;
             //     }
             // }
         }
-
     }
 
     return times;
